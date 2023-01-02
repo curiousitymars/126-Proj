6fd08a234c0f3d742847b734fa9421fa1fdacd2c
curiousitymars <84633190+curiousitymars@users.noreply.github.com>
2023-01-02 12:10:31 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+music = 0;
+music2 = 0;
+function preload() {
+    music = loadSound("music.mp3");
+    music2 = loadSound("music2.mp3");
+}
+function setup() {
+    canvas = createCanvas(600, 600);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+    image(video, 0, 0, 600, 600);
+}
